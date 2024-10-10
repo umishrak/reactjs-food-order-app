b8ede90d76b4976df9ab301eecc006e2ee0a0f5a
Utpal Kant Mishra <authorutpal@gmail.com>
2024-10-10 12:43:13 +0530

    add cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import cart from './components/Cart/Cart';
 
 function App() {
   return (
     <CartProvider>
+      <cart />
       <Header />
       <main>
         <Meals />
